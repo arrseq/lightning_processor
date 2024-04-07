020c24e7c408d05bfd4c677cc2c1909035e3b28e
rayyankhan1428 <rayyankhan1428h@gmail.com>
2024-04-06 21:59:43 -0400

    Implemented opcode reading

--- src/architecture/examples/scratch.rs
+++ src/architecture/examples/scratch.rs
@@ -1,6 +1,21 @@
 use std::io::Cursor;
 
+use architecture::instruction::{OperandPresense, Parser};
+
 fn main() {
     let bytes = vec![0, 0, 1];
-    let rom = Cursor::new(bytes);
+    let mut rom = Cursor::new(bytes);
+
+    let mut parser = Parser::new(0, OperandPresense {
+        source0: true,
+        source1: false,
+        destination: true,
+    });
+
+    let instruction = match parser.parse(&mut rom) {
+        Err(err) => return println!("Failed to parse to instruction {:?}", err),
+        Ok(ins) => ins
+    };
+
+    println!("Parsed to instruction, opcode {}", instruction.operation);
 }
--- src/architecture/src/instruction.rs
+++ src/architecture/src/instruction.rs
@@ -1,41 +1,58 @@
-pub enum Operation {
-    SetRegister,  // 0, d, r0
-    CloneRegister // 1, d, r0
-}
+use std::{fmt::Display, io::Read};
 
 pub struct OperandPresense {
-    register0: bool,
-    register1: bool,
-    destination: bool
+    pub source0: bool,
+    pub source1: bool,
+    pub destination: bool
 }
 
 pub struct Instruction {
-    operation: Operation,
-    register0: Option<u8>,
-    register1: Option<u8>,
-    destination: Option<u8>
+    pub operation: u8,
+    pub source0: Option<u8>,
+    pub source1: Option<u8>,
+    pub destination: Option<u8>
 }
 
+#[derive(Debug)]
 pub enum Error {
     EndOfStream,
     OperandUnmatched
 }
 
 pub struct Parser {
-    operation: Operation,
+    operation: u8,
     operand_presense: OperandPresense
 }
 
 impl Parser {
-    pub fn new(operation: Operation, operand_presense: OperandPresense) -> Self {
+    pub fn new(operation: u8, operand_presense: OperandPresense) -> Self {
         Parser {
             operation,
             operand_presense
         }
     }
 
-    pub fn parse(&mut self) -> Result<Instruction, Error> {
-        
-        Err(Error::EndOfStream)
+    pub fn parse(&mut self, source: &mut dyn Read) -> Result<Instruction, Error> {
+        let mut buffer = [0 as u8; 1];
+        let opcode_bytes_received = match source.read(&mut buffer) {
+            Err(_) => return Err(Error::EndOfStream),
+            Ok(value) => value
+        };
+
+        if opcode_bytes_received == 0 {
+            return Err(Error::EndOfStream);
+        }
+
+        let received_operation = buffer[0];
+        if received_operation != self.operation {
+            return Err(Error::OperandUnmatched);
+        }
+
+        Ok(Instruction {
+            operation: received_operation,
+            source0: None,
+            source1: None,
+            destination: None
+        })
     }
 }
