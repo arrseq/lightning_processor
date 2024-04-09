85a64c7f8d49c8afbb84be8803ca4e1c04702704
rayyankhan1428 <rayyankhan1428h@gmail.com>
2024-04-09 10:49:20 -0400

    Implemented system to read single units of data

--- src/architecture/examples/scratch.rs
+++ src/architecture/examples/scratch.rs
@@ -1,28 +1,40 @@
 use std::io::Cursor;
 
-use architecture::instruction::{OperandPresense, Parser};
+use architecture::instruction::{read_sized_block, OperandPresense, Parser};
 
 fn main() {
     let bytes = vec![0, 20, 15, 0, 100, 47];
     let mut rom = Cursor::new(bytes);
 
-    let mut parser = Parser::new(0, OperandPresense {
-        source0: true,
-        source1: false,
-        destination: true,
-    });
+    let data = vec![2, 5, 10];
+    let mut data_rom = Cursor::new(data);
 
-    let instruction = match parser.parse(&mut rom) {
-        Err(err) => panic!("Could not parse"),
-        Ok(ins) => ins
-    };
+    let parsed = read_sized_block(&mut data_rom);
+    if parsed.is_some() {
+        for el in parsed.unwrap() {
+            println!("Byte: {}", el);
+        }
+    } else {
+        println!("Err None received");
+    }
 
-    println!("Parsed to instruction, opcode {}, dest {:?}, source 0 {:?}, source 1 {:?}", instruction.operation, instruction.destination, instruction.source0, instruction.source1);
+    // let mut parser = Parser::new(0, OperandPresense {
+    //     source0: true,
+    //     source1: false,
+    //     destination: true,
+    // });
 
-    let instruction = match parser.parse(&mut rom) {
-        Err(err) => panic!("Could not parse"),
-        Ok(ins) => ins
-    };
+    // let instruction = match parser.parse(&mut rom) {
+    //     Err(err) => panic!("Could not parse"),
+    //     Ok(ins) => ins
+    // };
 
-    println!("Parsed to instruction, opcode {}, dest {:?}, source 0 {:?}, source 1 {:?}", instruction.operation, instruction.destination, instruction.source0, instruction.source1);
+    // println!("Parsed to instruction, opcode {}, dest {:?}, source 0 {:?}, source 1 {:?}", instruction.operation, instruction.destination, instruction.source0, instruction.source1);
+
+    // let instruction = match parser.parse(&mut rom) {
+    //     Err(err) => panic!("Could not parse"),
+    //     Ok(ins) => ins
+    // };
+
+    // println!("Parsed to instruction, opcode {}, dest {:?}, source 0 {:?}, source 1 {:?}", instruction.operation, instruction.destination, instruction.source0, instruction.source1);
 }
--- src/architecture/src/instruction.rs
+++ src/architecture/src/instruction.rs
@@ -15,7 +15,7 @@ pub struct Instruction {
 
 pub enum InstructionParseError {
     EndOfStream,
-    OperandUnmatched
+    OperationUnmatched
 }
 
 pub struct Parser {
@@ -32,14 +32,7 @@ impl Parser {
     }
 
     pub fn parse(&mut self, source: &mut dyn Read) -> Result<Instruction, InstructionParseError> {
-        let mut buffer = [0 as u8; 4];
-        let bytes_received = match source.read(&mut buffer) {
-            Err(_) => return Err(InstructionParseError::EndOfStream),
-            Ok(value) => value
-        };
-        if bytes_received == 0 {
-            return Err(InstructionParseError::EndOfStream);
-        }
+        let mut buffer = [0 as u8; 1];
 
         let mut received_operation: u8 = 0;
         let mut received_source0: Option<u8> = None;
@@ -53,11 +46,20 @@ impl Parser {
             + self.operand_presense.source1 as u8;
 
         for _ in 0..expected {
+            let bytes_received = match source.read(&mut buffer) {
+                Err(_) => return Err(InstructionParseError::EndOfStream),
+                Ok(value) => value
+            };
+
+            if bytes_received == 0 {
+                return Err(InstructionParseError::EndOfStream);
+            }
+
             let value = buffer[0];
             match byte_index {
                 0 => {
                     if value != self.operation {
-                        return Err(InstructionParseError::OperandUnmatched);
+                        return Err(InstructionParseError::OperationUnmatched);
                     }
 
                     received_operation = value;
@@ -80,9 +82,31 @@ impl Parser {
     }
 }
 
-// Parse an integer in a byte stream with a specified length. If None is
-// returned then that indicates that the stream did not meet the expected
-// length. 
-pub fn parse_dynamic_integer(byte_stream: &mut dyn Read) -> Option<usize> {
-    // let buffer = [0 as u8]
+pub fn read_sized_block(byte_stream: &mut dyn Read) -> Option<Vec<u8>> {
+    let mut buffer = [0 as u8; 1];
+    match byte_stream.read(&mut buffer) {
+        Err(_) => return None,
+        Ok(bytes_received) => {
+            if bytes_received == 0 {
+                return None;
+            }
+        }
+    };
+    let value_size = buffer[0];
+
+    let mut bytes: Vec<u8> = Vec::new();
+    for _ in 0..value_size {
+        match byte_stream.read(&mut buffer) {
+            Err(_) => return None,
+            Ok(length) => {
+                if length == 0 {
+                    return None;
+                }
+            }
+        };
+
+        bytes.push(buffer[0]);
+    }
+    
+    Some(bytes) 
 }
