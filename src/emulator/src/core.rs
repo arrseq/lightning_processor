3eb434b5c360424b8cedbe6c93df92f18cc9ce1f
Rayyan Khan <rayyankhan1428h@gmail.com>
2024-04-18 23:48:01 -0400

    Implemented basic core and register

---
+++ src/emulator/src/core.rs
@@ -0,0 +1,28 @@
+use crate::register;
+
+pub struct Core {
+    pub registers: register::File,
+    safe: bool
+}
+
+impl Core {
+    pub fn new() -> Self {
+        Core {
+            safe: false,
+            registers: register::File::new()
+        }
+    }
+
+    pub fn perform_register(&mut self) {
+        // self.registers.
+        unimplemented!()
+    }
+
+    pub fn is_safe(&self) -> bool {
+        self.safe
+    }
+
+    pub fn set_safe(&mut self, safe: bool) {
+        unimplemented!()
+    }
+}
