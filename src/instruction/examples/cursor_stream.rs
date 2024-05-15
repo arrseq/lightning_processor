9e4c41900b6bc777e291ddea7f5cdd6ed81eb33e
= <rayyankhan1428h@gmail.com>
2024-05-14 23:50:15 -0400

    [#35] docs(decoder) lib(macros)
    Co-authored-by: Logie457 <Logie457@users.noreply.github.com>

--- src/instruction/examples/cursor_stream.rs
+++ src/instruction/examples/cursor_stream.rs
@@ -1,5 +1,6 @@
 use std::io::Cursor;
 use em_instruction::{Instruction, operand};
+use em_instruction::coder::decoder::decode;
 use em_instruction::operand::{Destination, Operands};
 use em_instruction::operation::{Classification, Logical};
 
@@ -12,7 +13,12 @@ fn main() {
 		}
 	};
 
-	let c_stream = Cursor::new([
-
+	let mut c_stream = Cursor::new([
+		0x00
 	]);
+	
+	match decode(&mut c_stream, &mut target_ins) {
+		Err(error) => panic!("Failed to decode instruction: {:?}", error),
+		Ok(_) => println!("Ok")
+	}
 }
--- src/instruction/src/coder.rs
+++ src/instruction/src/coder.rs
@@ -1,2 +1,16 @@
+//! Instruction coding format is as follows in the same order (separated into clusters of a single byte)
+//! - Classification
+//! - Destination
+//! ---
+//! - Operation
+//! - Addressing Method
+//! - Addressing Mode
+//! ---
+//! - Exponent
+//! - First Register
+//! - Second Register
+//! ---
+//! - Immediate 0-8 bytes
+
 pub mod decoder;
 pub mod encoder;
