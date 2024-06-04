65d24d8abf162d9c84f1e1e8715dfe444aea6a40
= <rayyankhan1428h@gmail.com>
2024-06-04 10:27:01 -0400

    [#35] test(operand) Moved tests to configured test module

--- src/instruction/src/operand.rs
+++ src/instruction/src/operand.rs
@@ -45,26 +45,6 @@ pub enum Operands {
 
 impl Operands {
 	/// Try to get the static operand.
-	/// ```
-	/// use em_instruction::absolute::Data;
-	/// use em_instruction::operand::{AllPresent, Dynamic, Operands, Static};
-	///
-	/// let x_static = 5;
-	///
-	/// let all = Operands::AllPresent(AllPresent {
-	///     x_static,
-	///     x_dynamic: Dynamic::Constant(Data::Byte(5))
-	/// });
-	///
-	/// let static_only = Operands::Static(x_static);
-	/// let dynamic_only = Operands::Dynamic(Dynamic::Constant(Data::Byte(5)));
-	/// let none = Operands::None;
-	///
-	/// assert_eq!(all.x_static().unwrap(), x_static);
-	/// assert_eq!(static_only.x_static().unwrap(), x_static);
-	/// assert!(dynamic_only.x_static().is_none());
-	/// assert!(none.x_static().is_none());
-	/// ```
 	pub fn x_static(&self) -> Option<Static> {
 		Some(match self {
 			Self::Static(x_static) => *x_static,
@@ -74,31 +54,55 @@ impl Operands {
 	}
 
 	/// Try to get the dynamic operand.
-	/// ```
-	/// use em_instruction::absolute::Data;
-	/// use em_instruction::operand::{AllPresent, Dynamic, Operands, Static};
-	///
-	/// let x_dynamic = Dynamic::Constant(Data::Byte(5));
-	///
-	/// let all = Operands::AllPresent(AllPresent {
-	///     x_static: 10,
-	///     x_dynamic: x_dynamic.clone()
-	/// });
-	///
-	/// let static_only = Operands::Static(10);
-	/// let dynamic_only = Operands::Dynamic(x_dynamic.clone());
-	/// let none = Operands::None;
-	///
-	/// assert_eq!(*all.x_dynamic().unwrap(), x_dynamic);
-	/// assert_eq!(*dynamic_only.x_dynamic().unwrap(), x_dynamic);
-	/// assert!(static_only.x_dynamic().is_none());
-	/// assert!(none.x_dynamic().is_none());
-	/// ```
 	pub fn x_dynamic(&self) -> Option<&Dynamic> {
 		Some(match self {
 			Self::Dynamic(x_dynamic) => x_dynamic,
 			Self::AllPresent(x_all) => &x_all.x_dynamic,
 			_ => return None
 		})
+	}
+}
+
+#[cfg(test)]
+mod operands_test {
+	use crate::absolute;
+	use crate::operand::{AllPresent, Dynamic, Operands};
+
+	#[test]
+	fn x_static() {
+		let x_static = 5;
+		
+		let all = Operands::AllPresent(AllPresent {
+		    x_static,
+		    x_dynamic: Dynamic::Constant(absolute::Data::Byte(5))
+		});
+		
+		let static_only = Operands::Static(x_static);
+		let dynamic_only = Operands::Dynamic(Dynamic::Constant(absolute::Data::Byte(5)));
+		let none = Operands::None;
+		
+		assert_eq!(all.x_static().unwrap(), x_static);
+		assert_eq!(static_only.x_static().unwrap(), x_static);
+		assert!(dynamic_only.x_static().is_none());
+		assert!(none.x_static().is_none());
+	}
+	
+	#[test]
+	fn x_dynamic() {
+		let x_dynamic = Dynamic::Constant(absolute::Data::Byte(5));
+		
+		let all = Operands::AllPresent(AllPresent {
+		    x_static: 10,
+		    x_dynamic: x_dynamic.clone()
+		});
+		
+		let static_only = Operands::Static(10);
+		let dynamic_only = Operands::Dynamic(x_dynamic.clone());
+		let none = Operands::None;
+		
+		assert_eq!(*all.x_dynamic().unwrap(), x_dynamic);
+		assert_eq!(*dynamic_only.x_dynamic().unwrap(), x_dynamic);
+		assert!(static_only.x_dynamic().is_none());
+		assert!(none.x_dynamic().is_none());
 	}
 }
