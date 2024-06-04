f2336da5bfd710324aa265a4d15e999e656670c6
= <rayyankhan1428h@gmail.com>
2024-06-03 20:58:45 -0400

    [#35] feat(lib) Implemented `extract_extension` & `set_extension`

--- readme.md
+++ readme.md
@@ -1,36 +1,19 @@
-# Processor [Emulator + SystemVerilog]
-Hardware and software for decoding, emulating, manufacturing the processor and its code.
+# Processor
+Execute instructions, manage memory, run programs. Provides hardware, compiler, and assembler.
 
-> [!NOTE]
-> ### Who is Logie457?
-> He is my friend who Is helping me develop this and many other projects. Currently still teaching him Rust and computer architecture.
+> Includes memory, compiler syntax libs, and more while this project is pre-bootstrapped. 
+> 
+> Post Bootstrap:
+> - Move syntax parser vcs2://AtomLine/Syntax
 
-> [!NOTE]
-> This is a modern CPU architecture, so you may expect a microcode engine to exist but there will never be one for this CPU. The reason is there is simply no good use or high enough benefits to use it. It reduces performance and was mainly to prevent hardwiring. But now CPUs are made in Verilog so this becomes unnecessary.
-https://discord.gg/4zt8sUEbNy
+# Specifications
+- 64-bit data bus with the ability to downsize to 32 bits. 
+- Complex instruction set (CISC)
+- Versatile instruction encoding and x86 translatable.
 
-### [Documentation](./docs.md)
-> [!IMPORTANT] 
-> The documentation is currently not finish or developed yet.
-
-Read information for how to write code for the processor, how to emulate, and more. 
-
-### Setting up your development environment
-<!-- TODO: Add OpenCL once it is used in the emulation core -->
-This project is written in [Rust](https://www.rust-lang.org/). Download [Rust Up](https://rustup.rs/) to quickly setup your environment. Follow the steps below to get the processor running on your system.
-
-Clone the processor to your computer and enter the directory.
-```bash
-> mkdir The_Four_Elements
-> cd The_Four_Elements
-> git clone https://github.com/tfe-exr/Processor
-> cd Processor
-```
-
-Download the dependencies and build the project.
-```bash
-> cargo build
-```
-
-### License
-There is no official for this project. We own the code and you may clone, fork, or take the code as a copy and not face legal repercussions.
+# Started
+- [x] Instruction decoder
+- [ ] Emulation 
+- [ ] Assembler 
+- [ ] Syntax parser 
+- [ ] Compiler
--- src/instruction/src/lib.rs
+++ src/instruction/src/lib.rs
@@ -1,28 +1,46 @@
+//! Binary instruction format is as follows.
+//! TODO: Explain each further
+//! Driver 0:
+//! - Extension
+//! - Lock
+//! - Destination Dynamic 
+//! 
+//! Driver 1
+//! - Operation
+//! - Addressing
+//! - Addressing Parameter
+//! 
+//! Data:
+//! - Width
+//! - Static Operand
+//! - Dynamic Operand
+//! 
+//! Immediate 0..8 quantized to 0, 2, 4 and 8.
+
 #![allow(clippy::unusual_byte_groupings)]
 
 pub mod absolute;
 pub mod operand;
 pub mod operation;
 
+use std::io;
 use std::io::Read;
 use crate::operand::{Operand, Operands};
 use crate::operation::{Extension, ExtensionFromCodeInvalid};
 
-// Bit masks for decoding instructions.
+// region: Binary instruction bit masks
+pub const DRIVER0_EXTENSION_MASK     : u8 = 0b111111_0_0;
+pub const DRIVER0_LOCK_MASK          : u8 = 0b000000_1_0;
+pub const DRIVER0_DESTINATION_DYNAMIC: u8 = 0b000000_0_1;
 
-pub const DRIVER0_EXTENSION_MASK  : u8 = 0b111111_0_0;
-pub const DRIVER0_LOCK_MASK       : u8 = 0b000000_1_0;
-pub const DRIVER0_DESTINATION_MASK: u8 = 0b000000_0_1;
+pub const DRIVER1_OPERATION_MASK      : u8 = 0b1111_00_00;
+pub const DRIVER1_ADDRESSING_MASK     : u8 = 0b0000_11_00;
+pub const DRIVER1_ADDRESSING_PARAMETER: u8 = 0b0000_00_11;
 
-pub const DRIVER1_OPERATION_MASK  : u8 = 0b1111_00_00;
-pub const DRIVER1_ADDRESSING_MASK : u8 = 0b0000_11_00;
-pub const DRIVER1_ADDRESSING_PARAM: u8 = 0b0000_00_11;
-
-pub const REGISTER_WIDTH_MASK  : u8 = 0b11_000_000;
-pub const REGISTER_STATIC_MASK : u8 = 0b00_111_000;
-pub const REGISTER_DYNAMIC_MASK: u8 = 0b00_000_111;
-
-// Instruction implementation, decoder and utilities
+pub const DATA_WIDTH_MASK          : u8 = 0b11_000_000;
+pub const DATA_STATIC_OPERAND_MASK : u8 = 0b00_111_000;
+pub const DATA_DYNAMIC_OPERAND_MASK: u8 = 0b00_000_111;
+// endregion
 
 /// The operand to dereference store the operation result in.
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -31,35 +49,19 @@ pub enum Destination {
     Dynamic
 }
 
-/// ```
-/// use em_instruction::{absolute, Destination, Instruction};
-/// use em_instruction::operand::{AllPresent, Dynamic, Operands, Static};
-/// use em_instruction::operation::arithmetic::Arithmetic;
-/// use em_instruction::operation::Extension;
-///
-/// let operation = Instruction {
-///     operation:     Extension::Arithmetic(Arithmetic::Add),
-///     width:         absolute::Type::Byte,
-///     destination:   Destination::Static, // Store value in r0
-///     operands:      Operands::AllPresent(AllPresent {
-///         x_static:  0, // r0 target
-///         x_dynamic: Dynamic::Constant(absolute::Data::Byte(5))
-///     })
-/// };
-/// ```
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Instruction {
     pub operation: Extension,
     /// Width of operands when dereferenced and for storing result.
     pub width: absolute::Type,
     pub destination: Destination,
-    pub operands: Operands
+    pub operands: Operands,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug)]
 pub enum DecodeError {
     /// Caused by the stream.
-    Stream,
+    Stream(io::Error),
     /// The extension of operation code was invalid.
     InvalidCode(ExtensionFromCodeInvalid)
 }
@@ -73,9 +75,81 @@ pub enum DestinationError {
     Dynamic
 }
 
+/// Structured data from the driver bytes.
+pub struct Driver {
+    /// Operation extension
+    pub extension: u8,
+    pub operation: u8,
+    /// Whether to store the data where the dynamic operand points if its addressing mode supports it.
+    pub destination_dynamic: bool,
+    /// Addressing mode of the dynamic operand
+    pub addressing: u8,
+    /// Parameter for the addressing mode.
+    pub addressing_parameter: u8
+}
+
+impl Driver {
+    /// Extract instruction driver parameters.
+    /// ```
+    /// use em_instruction::Driver;
+    /// 
+    /// 
+    /// let driver0 = 0b001010_0_0;
+    /// let driver1 = 0b1111_00_00;
+    ///
+    /// let driver = Driver::from_bytes([driver0, 0]);
+    ///
+    /// assert_eq!(driver.extension, 10);
+    /// ```
+    pub fn from_bytes(bytes: [u8; 2]) -> Self {
+        let driver0 = bytes[0];
+        let driver1 = bytes[1];
+
+        Driver {
+            extension: 0,
+            operation: 0,
+            destination_dynamic: false,
+            addressing: 0,
+            addressing_parameter: 0,
+        }
+    }
+    
+    /// ```
+    /// use em_instruction::Driver;
+    ///
+    /// assert_eq!(Driver::extract_extension(0b001101_0_0), 0b00_001101);
+    /// assert_eq!(Driver::extract_extension(0b101010_0_1), 0b00_101010);
+    /// ```
+    pub fn extract_extension(driver0: u8) -> u8 {
+        (DRIVER0_EXTENSION_MASK & driver0) >> 2
+    }
+    
+    /// First 6 bits of the extension code are use for.
+    /// ```
+    /// use em_instruction::Driver;
+    /// 
+    /// assert_eq!(Driver::set_extension(0b000000_0_1, 0b001010), 0b001010_0_1);
+    /// assert_eq!(Driver::set_extension(0b101100_0_0, 0b101100), 0b101100_0_0);
+    /// assert_eq!(Driver::set_extension(0b101100_1_0, 0b101100), 0b101100_1_0);
+    /// 
+    /// // Truncating extension
+    /// assert_eq!(Driver::set_extension(0b00000000_0_0, 0b11_111111), 0b111111_0_0);
+    /// assert_eq!(Driver::set_extension(0b00000000_0_1, 0b11_111110), 0b111110_0_1);
+    /// ```
+    pub fn set_extension(driver0: u8, extension: u8) -> u8 {
+        let extension_layer = (0b00_111111 & extension) << 2;
+        (driver0 & !DRIVER0_EXTENSION_MASK) | extension_layer
+    }
+}
+
 impl Instruction {
-    // Decode a binary stream into an instruction then store it in the target.
-    pub fn decode(stream: &mut impl Read, target: &mut Instruction) -> Result<(), DecodeError> {
+    // Decode a binary stream to update this instruction with the new data.
+    pub fn decode(&mut self, stream: &mut dyn Read) -> Result<(), DecodeError> {
+        let buffer = [0u8; 1];
+
+        // Decode driver 0
+    
+
         Ok(())
     }
     
