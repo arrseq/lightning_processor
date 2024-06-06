b05eaf31d1f910318497a511117075dc7e55d82f
= <rayyankhan1428h@gmail.com>
2024-06-05 22:07:59 -0400

    [#35] feat(lib) Implemented re-encoding of instructions

--- src/instruction/examples/test.rs
+++ src/instruction/examples/test.rs
@@ -1,5 +1,6 @@
 use std::io::Cursor;
-use em_instruction::Instruction;
+use em_instruction::{absolute, Data, Instruction};
+use em_instruction::operand::{AllPresent, Dynamic, Operands};
 
 fn print_bytes(b: [u8; 2]) {
 	for x in b { print!("{:08b}, ", x) }
@@ -7,23 +8,27 @@ fn print_bytes(b: [u8; 2]) {
 }
 
 fn main() {
-	// let mut rom = Cursor::new([0, 0, 0, 0, 0, 0]); // Everything as 0 is a syntactically correct instruction.
-	// 
-	// let mut instruction;
-	// 
-	// loop {
-	// 	instruction = Instruction::from_encoded(&mut rom)
-	// 		.expect("Could not decode");
-	// 
-	// 	println!("{:?}", instruction);
-	// }
-	
-	let num: u16 = 0b11110000_10101010;
-	let nub = u16::from_le_bytes([0b10101010, 0b11110000]);
-	
-	println!("num: {:016b}", num);
-	println!("nub: {:016b}", nub);
-	
-	let num_bytes = u16::to_be_bytes(num);
-	print_bytes(num_bytes);
+	let mut rom = Cursor::new([0, 0, 0, 0, 0, 0]); // Everything as 0 is a syntactically correct instruction.
+
+	let mut instruction;
+
+	instruction = Instruction::from_encoded(&mut rom)
+		.expect("Could not decode");
+
+	if let Some(data) = instruction.data {
+		instruction.data = Some(Data {
+			width: data.width,
+			operands: Operands::AllPresent(AllPresent {
+				x_static: 3,
+				x_dynamic: Dynamic::Constant(absolute::Data::Quad(2))
+			}),
+			destination: data.destination,
+			synchronise: true
+		});
+	}
+
+	println!("{:?}", instruction);
+
+	let encoded = instruction.encode();
+	println!("{:?}", encoded);
 }
--- src/instruction/src/absolute.rs
+++ src/instruction/src/absolute.rs
@@ -2,9 +2,9 @@
 //! While Rust has u8, u16... for absolute values, it does not have a simple enum for variable length
 //! absolute integers. 
 
-// TODO: Use TryFrom<u8..u16..u32..u64> on Data
-
 // Constants
+
+use crate::operand::{IMMEDIATE_EXPONENT_BYTE, IMMEDIATE_EXPONENT_DUAL, IMMEDIATE_EXPONENT_QUAD, IMMEDIATE_EXPONENT_WORD};
 
 pub const BYTE_SIZE: u8 = 1;
 pub const WORD_SIZE: u8 = 2;
@@ -36,7 +36,7 @@ impl From<Data> for Type {
 }
 
 impl Type {
-	/// Create from the number of bytes.
+	/// Create from a number of bytes.
 	pub fn from_bytes(bytes: u8) -> Option<Self> {
 		Some(match bytes {
 			BYTE_SIZE => Type::Byte,
@@ -49,7 +49,16 @@ impl Type {
 	
 	/// Create from an exponent of 2. The maximum supported exponent is 3.
 	pub fn from_exponent(exponent: u8) -> Option<Self> {
-		Self::from_bytes(2 ^ exponent)
+		Self::from_bytes(2u8.pow(exponent as u32))
+	}
+
+	pub fn exponent(&self) -> u8 {
+		match self {
+			Self::Byte => IMMEDIATE_EXPONENT_BYTE,
+			Self::Word => IMMEDIATE_EXPONENT_WORD,
+			Self::Dual => IMMEDIATE_EXPONENT_DUAL,
+			Self::Quad => IMMEDIATE_EXPONENT_QUAD
+		}
 	}
 }
 
@@ -77,6 +86,34 @@ impl Data {
 		
 		bytes
 	}
+	
+	pub fn exponent(self) -> u8 {
+		Type::from(self).exponent()
+	}
+}
+
+impl From<u8> for Data {
+	fn from(value: u8) -> Self {
+		Self::Byte(value)
+	}
+}
+
+impl From<u16> for Data {
+	fn from(value: u16) -> Self {
+		Self::Word(value)
+	}
+}
+
+impl From<u32> for Data {
+	fn from(value: u32) -> Self {
+		Self::Dual(value)
+	}
+}
+
+impl From<u64> for Data {
+	fn from(value: u64) -> Self {
+		Self::Quad(value)
+	}
 }
 
 impl From<Type> for Data {
@@ -89,5 +126,3 @@ impl From<Type> for Data {
 		}
 	}
 }
-
-// TODO: Implement unit tests
