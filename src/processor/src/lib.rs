1052b14075023105130931ab5e2ff2f31c304d76
= <rayyankhan1428h@gmail.com>
2024-06-22 22:29:11 -0400

    docs(memory) Explained terms.

--- src/processor/src/lib.rs
+++ src/processor/src/lib.rs
@@ -1,3 +1,13 @@
+//! # Declarations
+//! - References to parts of a binary number will be in big endian. Saying left most bits would refer to the most 
+//!   significant bits.
+//! - The architecture this library implements is for the xT3 processor. xT3 also reefers to the architecture of the
+//!   processor.
+//! 
+//! # Terms
+//! - Real mode is a state of the processor that allows addressing to be as is rather than being translated. An 
+//!   operating system kernel would run in real mode.
+
 #![feature(let_chains)]
 #![forbid(clippy::result_unit_err)]
 #![forbid(clippy::question_mark)]
