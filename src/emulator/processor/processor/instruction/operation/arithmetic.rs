b9a2d7ee4a27ce90f81b98bfd19a6fa1cbde102a
= <rayyankhan1428h@gmail.com>
2024-07-03 18:37:16 -0400

    feat(arithmetic) Support for tripping flags and consuming them for some instructions.

--- src/emulator/processor/processor/instruction/operation/arithmetic.rs
+++ src/emulator/processor/processor/instruction/operation/arithmetic.rs
@@ -3,7 +3,7 @@ use emulator::memory::Memory;
 use emulator::processor::processor::{Context, ExternalContext, Ports, Registers};
 use emulator::processor::processor::instruction::operand::{Destination, Dynamic};
 use number;
-use number::{CarryingAdd, CarryingSub, CheckedAdd, CheckedMul, CheckedSub};
+use number::{CarryingAdd, CarryingSub, CheckedAdd, CheckedMul, CheckedSub, WrappingAdd, WrappingDiv, WrappingMul, WrappingSub};
 use crate::emulator::processor;
 use crate::emulator::processor::processor::instruction::Data;
 use crate::emulator::processor::processor::instruction::operand::OperandsPresence;
@@ -22,7 +22,9 @@ pub enum Arithmetic {
     Add,
     Subtract,
     Multiply, 
-    Divide
+    Divide,
+    CarryingAdd,
+    BorrowingSub,
 }
 
 /// Flags that can be set or will be set by this unit. Flags could be used by instructions that read the flags. Specific 
@@ -46,8 +48,8 @@ impl<'a> Operation<'a> for Arithmetic {
             .read(&data.width, &external_context.memory, context.virtual_mode, &context.registers).map_err(OperationExecuteError::DynamicRead)?;
         
         context.arithmetic_flags.overflow = match self {
-            Self::Add => r#static.checked_add(dynamic),
-            Self::Subtract => r#static.checked_sub(dynamic),
+            Self::Add | Arithmetic::CarryingAdd => r#static.checked_add(dynamic),
+            Self::Subtract | Arithmetic::BorrowingSub => r#static.checked_sub(dynamic),
             Self::Multiply => r#static.checked_mul(dynamic),
             Self::Divide => r#static.checked_mul(dynamic)
         }.is_none();
@@ -55,17 +57,26 @@ impl<'a> Operation<'a> for Arithmetic {
         context.arithmetic_flags.cary = match self {
             Self::Add => r#static.carrying_add(dynamic, false).unwrap().1,
             Self::Subtract => r#static.carrying_sub(dynamic, false).unwrap().1,
+            Self::CarryingAdd => r#static.carrying_add(dynamic, context.arithmetic_flags.cary).unwrap().1,
+            Self::BorrowingSub => r#static.carrying_add(dynamic, context.arithmetic_flags.cary).unwrap().1,
             _ => false
         };
         
-        context.arithmetic_flags.zero = todo!();
+        let result = match self {
+            Self::Add | Self::CarryingAdd => r#static.wrapping_add(dynamic),
+            Self::Subtract | Self::BorrowingSub => r#static.wrapping_sub(dynamic),
+            Self::Multiply => r#static.wrapping_mul(dynamic),
+            Self::Divide => r#static.wrapping_div(dynamic)
+        };
         
-        // match data.destination {
-        //     Destination::Static => *context.registers.get_mut(all_operands.x_static as usize).unwrap() = result.quad(),
-        //     Destination::Dynamic => all_operands.x_dynamic
-        //         .write(&data.width, &mut external_context.memory, context.virtual_mode, &mut context.registers, result)
-        //         .map_err(OperationExecuteError::DynamicRead)?
-        // };
+        context.arithmetic_flags.zero = result.is_zero();
+        
+        match data.destination {
+            Destination::Static => *context.registers.get_mut(all_operands.x_static as usize).unwrap() = result.quad(),
+            Destination::Dynamic => all_operands.x_dynamic
+                .write(&data.width, &mut external_context.memory, context.virtual_mode, &mut context.registers, result)
+                .map_err(OperationExecuteError::DynamicRead)?
+        };
         
         Ok(())
     }
@@ -81,7 +92,8 @@ impl Coded<u8> for Arithmetic {
             Self::Add      => ADD_CODE,
             Self::Subtract => SUBTRACT_CODE,
             Self::Multiply => MULTIPLY_CODE,
-            Self::Divide   => DIVIDE_CODE
+            Self::Divide   => DIVIDE_CODE,
+            _ => todo!() // TODO
         }
     }
 }
@@ -93,6 +105,7 @@ impl Arithmetic {
             SUBTRACT_CODE => Self::Subtract,
             MULTIPLY_CODE => Self::Multiply,
             DIVIDE_CODE   => Self::Divide,
+            // TODO
             _ => return None
         })
     }
