69fbe6404e753ecdc6e6a12ebf76bb84e549b05e
= <rayyankhan1428h@gmail.com>
2024-07-07 08:50:31 -0400

    feat(operand) Type conversions.   * Supported conversion from operand types to configuration.   * Supported decoding boolean to an operand type.   * Supported encoding an operand type to a boolean.

--- src/instruction/operand.rs
+++ src/instruction/operand.rs
@@ -19,7 +19,21 @@ pub enum Configuration {
     Dynamic(Dynamic)
 }
 
+impl From<Dual> for Configuration { fn from(value: Dual) -> Self { Self::Dual(value) } }
+impl From<Register> for Configuration { fn from(value: Register) -> Self { Self::Static(value) } }
+impl From<Dynamic> for Configuration { fn from(value: Dynamic) -> Self { Self::Dynamic(value) } }
+
 pub enum Type {
     Static,
     Dynamic
+}
+
+impl From<bool> for Type {
+    /// Convert a boolean to an operand type. If the value is [true] then [Type::Dynamic] is returned.
+    fn from(value: bool) -> Self { if value { Self::Dynamic } else { Self::Static } }
+}
+
+impl From<Type> for bool {
+    /// Convert an operand type to a boolean. If the operand is [Type::Dynamic] then [true] is returned.
+    fn from(value: Type) -> Self { matches!(value, Type::Dynamic) }
 }
