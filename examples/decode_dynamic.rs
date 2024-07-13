48df698f4e243be3bc02633126b77633fbd66fdf
= <rayyankhan1428h@gmail.com>
2024-07-13 13:56:45 -0400

    fix(example) Fixed example with broken imports.

--- examples/decode_dynamic.rs
+++ examples/decode_dynamic.rs
@@ -1,14 +1,14 @@
-extern crate arrseq_instruction;
+extern crate arrseq_lightning;
 
 use std::io::Cursor;
-use arrseq_instruction::instruction::Instruction;
+use arrseq_lightning::instruction::Instruction;
 
 fn main() {
     let mut cursor = Cursor::new([
         0x00,
         0x00,
         0b00_1_1110_1,
-        0b1110_0110,
+        0b1111_0110,
         100,
         100,
         100,
