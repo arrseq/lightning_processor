c85eb3a18cd0f2946374018fa366e69bfd973455
= <rayyankhan1428h@gmail.com>
2024-07-15 20:27:35 -0400

    fix(instruction) Encode operation and ensure sync mode is used with address operand.

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,6 +1,7 @@
 use std::io;
 use std::io::{Read, Write};
 use crate::dynamic_number;
+use crate::instruction::operand::dynamic::Dynamic;
 use crate::instruction::operand::Meta;
 use self::operand::Operands;
 use self::operation::Operation;
@@ -28,7 +29,11 @@ pub enum DecodeError {
 #[derive(Debug)]
 pub enum EncodeError {
     Write(io::Error),
-    Operands(operand::EncodeError)
+    Operands(operand::EncodeError),
+    
+    /// The instruction was set to execute synchronously but there was no address operand. Synchronous execution is used
+    /// to access a memory address without a race condition.
+    SynchronizedWithNoAddress
 }
 
 #[derive(Debug)]
@@ -68,14 +73,24 @@ impl Instruction {
     pub fn encode(self, output: &mut impl Write) -> Result<(), EncodeError> {
         let operation_escape = Self::get_operation_escape(self.operation).map_err(EncodeError::Write)?;
         
-        Prefixes {
+        let prefixes = Prefixes {
             escape: operation_escape,
             execution: self.execution,
             branch_likely_taken: self.branch_likely_taken
-        }
+        };
+        
+        // Synchronized instructions must have the dynamic operand point to an address.
+        if
+            // Has an execution mode override.
+            let Some(execution) = self.execution
+            // The execution mode is synchronous.
+            && let prefix::Execution::Synchronize = execution
+            // The dynamic addressing mode is not address.
+            && !matches!(self.operands.dynamic, Dynamic::Address(_)) { return Err(EncodeError::SynchronizedWithNoAddress) }
+        
+        prefixes
             .encode(output)
             .map_err(EncodeError::Write)?;
-        
         Self::encode_operation(output, self.operation, operation_escape).map_err(EncodeError::Write)?;
         self.operands.encode(output).map_err(EncodeError::Operands)?;
         
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![feature(seek_stream_len)]
+#![feature(let_chains)]
 
 pub mod dynamic_number;
 pub mod instruction;
