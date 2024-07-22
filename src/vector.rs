0c942c92b926d76dc10a6cf1955765c0ccf89fb1
= <rayyankhan1428h@gmail.com>
2024-07-22 19:13:40 -0400

    #78 refactor(vector) Updated naming convention.

--- src/vector.rs
+++ src/vector.rs
@@ -8,8 +8,8 @@ pub enum Component {
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct DoubleVector<Component> {
-    pub a: Component,
-    pub b: Component
+    pub component_a: Component,
+    pub component_b: Component
 }
 
 pub type U8DoubleVector = DoubleVector<u8>;
@@ -19,9 +19,9 @@ pub type U64DoubleVector = DoubleVector<u64>;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct TrioVector<Component> {
-    pub a: Component,
-    pub b: Component,
-    pub c: Component
+    pub component_a: Component,
+    pub component_b: Component,
+    pub component_c: Component
 }
 
 pub type U8TrioVector = TrioVector<u8>;
@@ -31,10 +31,10 @@ pub type U64TrioVector = TrioVector<u64>;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct QuartetVector<Component> {
-    pub a: Component,
-    pub b: Component,
-    pub c: Component,
-    pub d: Component
+    pub component_a: Component,
+    pub component_b: Component,
+    pub component_c: Component,
+    pub component_d: Component
 }
 
 pub type U8QuartetVector = QuartetVector<u8>;
