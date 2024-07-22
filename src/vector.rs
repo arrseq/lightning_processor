165e23d8df08c1a5f0777f75c142da38cb12ed47
= <rayyankhan1428h@gmail.com>
2024-07-22 19:10:33 -0400

    #78 refactor(vector) Updated naming convention.

--- src/vector.rs
+++ src/vector.rs
@@ -7,37 +7,37 @@ pub enum Component {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-pub struct N2ComponentVector<Component> {
+pub struct DoubleVector<Component> {
     pub a: Component,
     pub b: Component
 }
 
-pub type N2U8ComponentVector = N2ComponentVector<u8>;
-pub type N2U16ComponentVector = N2ComponentVector<u16>;
-pub type N2U32ComponentVector = N2ComponentVector<u32>;
-pub type N2U64ComponentVector = N2ComponentVector<u64>;
+pub type U8DoubleVector = DoubleVector<u8>;
+pub type U16DoubleVector = DoubleVector<u16>;
+pub type U32DoubleVector = DoubleVector<u32>;
+pub type U64DoubleVector = DoubleVector<u64>;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-pub struct N3ComponentVector<Component> {
+pub struct TrioVector<Component> {
     pub a: Component,
     pub b: Component,
     pub c: Component
 }
 
-pub type N3U8ComponentVector = N3ComponentVector<u8>;
-pub type N3U16ComponentVector = N3ComponentVector<u16>;
-pub type N3U32ComponentVector = N3ComponentVector<u32>;
-pub type N3U64ComponentVector = N3ComponentVector<u64>;
+pub type U8TrioVector = TrioVector<u8>;
+pub type U16TrioVector = TrioVector<u16>;
+pub type U32TrioVector = TrioVector<u32>;
+pub type U64TrioVector = TrioVector<u64>;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-pub struct N4ComponentVector<Component> {
+pub struct QuartetVector<Component> {
     pub a: Component,
     pub b: Component,
     pub c: Component,
     pub d: Component
 }
 
-pub type N4U8ComponentVector = N4ComponentVector<u8>;
-pub type N4U16ComponentVector = N4ComponentVector<u16>;
-pub type N4U32ComponentVector = N4ComponentVector<u32>;
-pub type N4U64ComponentVector = N4ComponentVector<u64>;
+pub type U8QuartetVector = QuartetVector<u8>;
+pub type U16QuartetVector = QuartetVector<u16>;
+pub type U32QuartetVector = QuartetVector<u32>;
+pub type U64QuartetVector = QuartetVector<u64>;
