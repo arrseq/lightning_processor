aa8170576712c0b61c93f54bb26bd119713aa277
= <rayyankhan1428h@gmail.com>
2024-07-22 23:30:03 -0400

    refactor(operand) Reduced code size by removing unnecessary intermediate structure in operand encoding/decoding process.

--- src/instruction/operand.rs
+++ src/instruction/operand.rs
@@ -23,67 +23,6 @@ pub enum Name {
     Dynamic
 }
 
-/// Metadata for the operand involving the size of the operands, addressing mode, and more.
-///
-/// Some fields are privately initiated to ensure the validity of the data.
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub struct Meta {
-    /// The size of the data being referenced by the operand(s).
-    pub size: dynamic_number::Size,
-
-    /// The name of the operand in which to store the result in.
-    pub destination: Name,
-
-    /// Whether to override the destination and use an external dynamic operand as the destination.
-    pub external_destination: bool,
-
-    /// The encoded code of the dynamic operand. The entire dynamic operand is not stored here to modularize the 
-    /// decoding process.
-    dynamic_code: u8,
-}
-
-/// The result of the meta points to the dynamic operand but the dynamic operand is constant data.
-#[derive(Debug, Error)]
-#[error("The result points to a constant")]
-pub struct ResultToConstantError;
-
-impl Meta {
-    /// # Result
-    /// Instance of [Self] as long as the dynamic operand isn't constant with the result of this metadata being set to
-    /// the dynamic operand.
-    pub fn new(size: dynamic_number::Size, destination: Name, custom_data: bool, dynamic: Dynamic) -> Result<Self, ResultToConstantError> {
-        if matches!(dynamic, Dynamic::Constant(_)) && matches!(destination, Name::Dynamic) { return Err(ResultToConstantError) }
-        Ok(Self {
-            size, destination,
-            external_destination: custom_data,
-            dynamic_code: dynamic.encode()
-        })
-    }
-
-    pub fn encode(self) -> u8 {
-        let mut encoded = self.size.exponent_representation() << 6;
-        encoded |= (matches!(self.destination, Name::Dynamic) as u8) << 5;
-        encoded |= self.dynamic_code << 1;
-        encoded |= self.external_destination as u8;
-        encoded
-    }
-
-    /// # Result
-    /// This function has no error because the dynamic code is never invalid. Valid dynamic codes are 4 bits.
-    pub fn decode(encoded: u8) -> Result<Self, dynamic::InvalidCodeError> {
-        let size = dynamic_number::Size::from_exponent_representation(encoded >> 6).unwrap();
-        let destination = if (encoded >> 5) & 0b0000000_1 == 1 { Name::Dynamic } else { Name::Register };
-        let dynamic_code = (encoded & 0b000_1111_0) >> 1;
-        if !Dynamic::is_valid(dynamic_code) { return Err(dynamic::InvalidCodeError); }
-        let custom_data = encoded & 0b0000000_1 == 1;
-        Ok(Self { size, destination, dynamic_code, external_destination: custom_data })
-    }
-
-    pub fn dynamic_code(self) -> u8 {
-        self.dynamic_code
-    }
-}
-
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Operands {
     /// The size of the data that the operands refer to.
@@ -103,10 +42,13 @@ pub enum DecodeError {
     Read(#[source] io::Error)
 }
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum EncodeError {
-    Write(io::Error),
-    ResultToConstant(ResultToConstantError)
+    #[error("")]
+    Write(#[source] io::Error),
+
+    #[error("The result points to a constant")]
+    ResultToConstant
 }
 
 impl Operands {
@@ -115,33 +57,40 @@ impl Operands {
         let mut buffer = [0u8; 2];
         input.read_exact(&mut buffer).map_err(DecodeError::Read)?;
 
-        // Code after this should unwrap on things that return [Err(dynamic::InvalidCodeError)] due to this.
-        let meta = Meta::decode(buffer[0]).map_err(DecodeError::InvalidDynamicCode)?;
+        // Decode meta data.
+        let meta_byte = buffer[0];
+        let meta_size = dynamic_number::Size::from_exponent_representation(meta_byte >> 6).unwrap();
+        let destination = if (meta_byte >> 5) & 0b0000000_1 == 1 { Name::Dynamic } else { Name::Register };
+        let dynamic_code = (meta_byte & 0b000_1111_0) >> 1;
+        if !Dynamic::is_valid(dynamic_code) { return Err(DecodeError::InvalidDynamicCode(dynamic::InvalidCodeError)); }
+        let external_destination = meta_byte & 0b0000000_1 == 1;
+        
+        // Decode the actual operands.
         let registers = register::Dual::decode(buffer[1]);
         
-        let dynamic = match Dynamic::requirement(meta.dynamic_code).unwrap() {
-            dynamic::Requirement::Register => Dynamic::decode_register(meta.dynamic_code, registers.second),
-            dynamic::Requirement::Constant(size) => Dynamic::decode_constant(meta.dynamic_code, Self::decode_constant(input, size.unwrap_or(meta.size)).map_err(DecodeError::Read)?),
+        let dynamic = match Dynamic::requirement(dynamic_code).unwrap() {
+            dynamic::Requirement::Register => Dynamic::decode_register(dynamic_code, registers.second),
+            dynamic::Requirement::Constant(size) => Dynamic::decode_constant(dynamic_code, Self::decode_constant(input, size.unwrap_or(meta_size)).map_err(DecodeError::Read)?),
             // There is no dynamic operand mode with this requirement that uses [None] for its constant size here. It is
             // acceptable to unwrap here.
             dynamic::Requirement::RegisterAndConstant(size) => {
                 let calculated = dynamic::Calculated {
                     base: registers.second,
-                    offset: Self::decode_constant(input, size.unwrap_or(meta.size)).map_err(DecodeError::Read)?
+                    offset: Self::decode_constant(input, size.unwrap_or(meta_size)).map_err(DecodeError::Read)?
                 };
                 
-                Dynamic::decode_calculated(meta.dynamic_code, calculated)
+                Dynamic::decode_calculated(dynamic_code, calculated)
             }
         }.unwrap();
         
-        let external_destination = if !meta.external_destination { None } 
+        let external_destination = if !external_destination { None } 
         else {
             todo!() as Option<Dynamic>
         };
         
         Ok(Self {
-            size: meta.size,
-            destination: meta.destination,
+            size: meta_size,
+            destination,
             register: registers.first,
             dynamic,
             external_destination
@@ -162,13 +111,19 @@ impl Operands {
     }
     
     pub fn encode(self, output: &mut impl Write) -> Result<(), EncodeError> {
-        let meta = Meta::new(self.size, self.destination, self.external_destination.is_some(), self.dynamic).map_err(EncodeError::ResultToConstant)?;
+        // Encode the operand meta data.
+        let mut encoded_meta = self.size.exponent_representation() << 6;
+        encoded_meta |= (matches!(self.destination, Name::Dynamic) as u8) << 5;
+        encoded_meta |= self.dynamic.encode() << 1;
+        encoded_meta |= self.external_destination.is_some() as u8;
+        
+        // Encode the actual operands.
         let registers = register::Dual {
             first: self.register,
             second: self.dynamic.register().unwrap_or_default()
         };
         
-        let buffer = [meta.encode(), registers.encode()];
+        let buffer = [encoded_meta, registers.encode()];
         output.write_all(&buffer).map_err(EncodeError::Write)?;
         
         if let Ok(constant) = self.dynamic.constant() { Self::encode_constant(output, constant).map_err(EncodeError::Write)?; }
