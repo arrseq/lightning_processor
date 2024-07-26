bcba97af3205a62a8c1a17147e1867a34757c880
= <rayyankhan1428h@gmail.com>
2024-07-26 12:32:31 -0400

    feat(operand) Implemented decoding functions.

--- src/instruction/operand.rs
+++ src/instruction/operand.rs
@@ -10,7 +10,7 @@ pub enum RegisterMode {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-pub enum ConstantBasedMode {
+pub enum ConstantMode {
     Constant,
     Relative,
     ArrayInObject
@@ -19,7 +19,7 @@ pub enum ConstantBasedMode {
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum SecondMode {
     Array,
-    ConstantBased { mode: ConstantBasedMode, constant: u64, mask: u64 }
+    ConstantBased { mode: ConstantMode, constant: u64, mask: u64 }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -36,40 +36,66 @@ pub enum Mode {
 /// specific mode in the first field which encodes more addressing modes which require more fields in the full encoding
 /// of an operand.
 #[derive(Debug, Clone, Copy, PartialEq)]
-pub struct EncodedMode(u8, Option<u8>);
+pub struct EncodedModes(pub u8, pub Option<u8>);
 
+/// # Reason
+/// An error used when a mode is invalid or invalid for a specific situation.
 #[derive(Debug, Error)]
-#[error("The operand mode code is invalid")]
-pub struct InvalidCodeError;
+#[error("The mode identifier code is invalid")]
+pub struct InvalidModeError;
 
 impl Mode {
     pub const REGISTER_MODE               : u8 = 0;
     pub const DEREFERENCE_REGISTER_MODE   : u8 = 1;
     pub const CONSTANT_MODE               : u8 = 2;
     pub const SECOND_MODE                 : u8 = 3;
-    
+
     pub const ARRAY_ADDRESSING_SECOND_MODE: u8 = 0;
     pub const CONSTANT_SECOND_MODE        : u8 = 1;
     pub const RELATIVE_SECOND_MODE        : u8 = 2;
     pub const ARRAY_IN_OBJECT_SECOND_MODE : u8 = 3;
 
     /// Encode this operand based on its mode.
-    pub fn encode_mode(self) -> EncodedMode {
+    pub fn encode_mode(self) -> EncodedModes {
         match self {
             Mode::Register { mode, .. } => match mode {
-                RegisterMode::Register => EncodedMode(Self::REGISTER_MODE, None),
-                RegisterMode::Dereference => EncodedMode(Self::DEREFERENCE_REGISTER_MODE, None)
+                RegisterMode::Register => EncodedModes(Self::REGISTER_MODE, None),
+                RegisterMode::Dereference => EncodedModes(Self::DEREFERENCE_REGISTER_MODE, None)
             },
-            Mode::Constant { .. } => EncodedMode(Self::CONSTANT_MODE, None),
+            Mode::Constant { .. } => EncodedModes(Self::CONSTANT_MODE, None),
             Mode::Second { mode, .. } => match mode {
-                SecondMode::Array => EncodedMode(Self::SECOND_MODE, Some(Self::ARRAY_ADDRESSING_SECOND_MODE)),
+                SecondMode::Array => EncodedModes(Self::SECOND_MODE, Some(Self::ARRAY_ADDRESSING_SECOND_MODE)),
                 SecondMode::ConstantBased { mode, .. } => match mode {
-                    ConstantBasedMode::Constant => EncodedMode(Self::SECOND_MODE, Some(Self::CONSTANT_SECOND_MODE)),
-                    ConstantBasedMode::Relative => EncodedMode(Self::SECOND_MODE, Some(Self::RELATIVE_SECOND_MODE)),
-                    ConstantBasedMode::ArrayInObject => EncodedMode(Self::SECOND_MODE, Some(Self::ARRAY_IN_OBJECT_SECOND_MODE))
+                    ConstantMode::Constant => EncodedModes(Self::SECOND_MODE, Some(Self::CONSTANT_SECOND_MODE)),
+                    ConstantMode::Relative => EncodedModes(Self::SECOND_MODE, Some(Self::RELATIVE_SECOND_MODE)),
+                    ConstantMode::ArrayInObject => EncodedModes(Self::SECOND_MODE, Some(Self::ARRAY_IN_OBJECT_SECOND_MODE))
                 }
             }
         }
+    }
+
+    pub fn decode_register_mode(first_mode: u8, register: u8) -> Result<Self, InvalidModeError> {
+        let mode_variant = match first_mode {
+            Self::REGISTER_MODE => RegisterMode::Register,
+            Self::DEREFERENCE_REGISTER_MODE => RegisterMode::Dereference,
+            _ => return Err(InvalidModeError)
+        };
+        
+        Ok(Self::Register { mode: mode_variant, register })
+    }
+
+    pub fn decode_constant_mode(second_mode: u8, constant: u64, constant_mask: u64, base_register: u8, index_register: u8) -> Result<Self, InvalidModeError> {
+        let mode_variant = match second_mode {
+            Self::CONSTANT_SECOND_MODE => ConstantMode::Constant,
+            Self::RELATIVE_SECOND_MODE => ConstantMode::Relative,
+            Self::ARRAY_IN_OBJECT_SECOND_MODE => ConstantMode::ArrayInObject,
+            _ => return Err(InvalidModeError)
+        };
+        
+        Ok(Self::Second { 
+            base_register, index_register,
+            mode: SecondMode::ConstantBased { mode: mode_variant, constant, mask: constant_mask }
+        })
     }
 }
 
@@ -92,7 +118,7 @@ impl Mode {
 /// - `constant_size`: Represents the size of the constant in bytes as a power of 2. This may be unused depending on 
 ///   the mode.
 /// 
-/// If the second addressing mode codes for a constant to be read, then the final bytes are read to code for the 
+/// If the second addressing mode codes for a constant to be read, then the final bytes are read to code for the
 /// constant.
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Operand {
--- src/instruction/operand/test.rs
+++ src/instruction/operand/test.rs
@@ -1,11 +1,11 @@
-use crate::instruction::operand::{ConstantBasedMode, EncodedMode, Mode, RegisterMode, SecondMode};
+use crate::instruction::operand::{ConstantMode, EncodedModes, Mode, RegisterMode, SecondMode};
 
 #[test]
 fn encode_mode() {
-    assert_eq!(Mode::Register { mode: RegisterMode::Register, register: 2 }.encode_mode(), EncodedMode(Mode::REGISTER_MODE, None));
+    assert_eq!(Mode::Register { mode: RegisterMode::Register, register: 2 }.encode_mode(), EncodedModes(Mode::REGISTER_MODE, None));
     assert_eq!(Mode::Second { 
-        mode: SecondMode::ConstantBased { mode: ConstantBasedMode::Relative, constant: 2, mask: u8::MAX as u64 },
+        mode: SecondMode::ConstantBased { mode: ConstantMode::Relative, constant: 2, mask: u8::MAX as u64 },
         base_register: 4,
         index_register: 8
-    }.encode_mode(), EncodedMode(Mode::SECOND_MODE, Some(Mode::RELATIVE_SECOND_MODE)));
+    }.encode_mode(), EncodedModes(Mode::SECOND_MODE, Some(Mode::RELATIVE_SECOND_MODE)));
 }
