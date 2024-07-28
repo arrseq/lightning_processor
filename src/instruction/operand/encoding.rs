19f98517a21187ec9cbb2cea9267798600723f91
= <rayyankhan1428h@gmail.com>
2024-07-27 22:46:40 -0400

    refactor(operand) Moved encode of mode into different function.

--- src/instruction/operand/encoding.rs
+++ src/instruction/operand/encoding.rs
@@ -20,30 +20,9 @@ impl SecondMode {
     }
 }
 
-impl Mode {
-    /// Encode this operand based on its mode.
-    fn encode_mode(self) -> EncodedModes {
-        match self {
-            Mode::Register { mode, .. } => match mode {
-                RegisterMode::Register => EncodedModes(Self::REGISTER_MODE, None),
-                RegisterMode::Dereference => EncodedModes(Self::DEREFERENCE_REGISTER_MODE, None)
-            },
-            Mode::Constant { .. } => EncodedModes(Self::CONSTANT_MODE, None),
-            Mode::Second { mode, .. } => match mode {
-                SecondMode::Array => EncodedModes(Self::SECOND_MODE, Some(SecondMode::ARRAY_ADDRESSING_MODE)),
-                SecondMode::ConstantBased { mode, .. } => match mode {
-                    ConstantMode::Constant => EncodedModes(Self::SECOND_MODE, Some(SecondMode::CONSTANT_MODE)),
-                    ConstantMode::Relative => EncodedModes(Self::SECOND_MODE, Some(SecondMode::RELATIVE_MODE)),
-                    ConstantMode::ArrayInObject => EncodedModes(Self::SECOND_MODE, Some(SecondMode::ARRAY_IN_OBJECT_MODE))
-                }
-            }
-        }
-    }
-}
-
 /// # Usage
 /// This can be used to encode the first and second mode byte because they both follower the same field types.
-/// 
+///
 /// # Parameters
 /// - The 4 least significant bits are used from the register.
 /// - The first 2 least significant bits are used from the first_mode and operand size.
@@ -70,14 +49,14 @@ impl ModeByte {
         encoded |= self.size & 0b000000_11;
         encoded
     }
-    
+
     fn decode(encoded: u8) -> Self {
         let register = (encoded & 0xF0) >> 4;
         let mode = (encoded & 0b0000_11_00) >> 2;
         let size = encoded & 0b000000_11;
         Self { register, mode, size }
     }
-    
+
     fn decode_size(self) -> Size {
         Size::from_power(self.size)
     }
@@ -104,17 +83,31 @@ pub enum DecodeError {
 impl Operand {
     fn encode(self, output: &mut impl Write) -> io::Result<()> {
         let registers = self.mode.registers().unwrap_or(EncodedRegisters(0, Some(0)));
-        let modes = self.mode.encode_mode();
+        let modes = match self.mode {
+            Mode::Register { mode, .. } => match mode {
+                RegisterMode::Register => EncodedModes(Mode::REGISTER_MODE, None),
+                RegisterMode::Dereference => EncodedModes(Mode::DEREFERENCE_REGISTER_MODE, None)
+            },
+            Mode::Constant { .. } => EncodedModes(Mode::CONSTANT_MODE, None),
+            Mode::Second { mode, .. } => match mode {
+                SecondMode::Array => EncodedModes(Mode::SECOND_MODE, Some(SecondMode::ARRAY_ADDRESSING_MODE)),
+                SecondMode::ConstantBased { mode, .. } => match mode {
+                    ConstantMode::Constant => EncodedModes(Mode::SECOND_MODE, Some(SecondMode::CONSTANT_MODE)),
+                    ConstantMode::Relative => EncodedModes(Mode::SECOND_MODE, Some(SecondMode::RELATIVE_MODE)),
+                    ConstantMode::ArrayInObject => EncodedModes(Mode::SECOND_MODE, Some(SecondMode::ARRAY_IN_OBJECT_MODE))
+                }
+            }
+        };
 
         // Write the first mode byte.
         let byte = ModeByte { register: registers.0, mode: modes.0, size: self.data_size.to_power() };
         output.write_all(&[byte.encode()])?;
-        
+
         self.encode_second_mode_byte(output)?;
-        
+
         Ok(())
     }
-    
+
     fn encode_second_mode_byte(self, output: &mut impl Write) -> io::Result<()> {
         // Get the constant and write the second mode byte if applicable.
         let constant = if let Mode::Second { mode, index_register, .. } = self.mode {
@@ -130,10 +123,10 @@ impl Operand {
         else { None };
 
         if let Some(constant) = constant { Self::encode_constant(output, constant)?; }
-        
+
         Ok(())
     }
-    
+
     fn encode_constant(output: &mut impl Write, constant: DynamicNumber) -> io::Result<()> {
         match constant {
             DynamicNumber::U8(value) => output.write_all(&[value])?,
@@ -141,18 +134,18 @@ impl Operand {
             DynamicNumber::U32(value) => output.write_all(&value.to_le_bytes())?,
             DynamicNumber::U64(value) => output.write_all(&value.to_le_bytes())?
         }
-        
+
         Ok(())
     }
-    
+
     fn decode(self, input: &mut impl Read) -> Result<Self, DecodeError> {
         let mut buffer = [0u8; 1];
         input.read_exact(&mut buffer).map_err(|io| DecodeError::Io { io, decoder: DecodeIoError::FirstModeByte })?;
-        
+
         let first_mode_byte = ModeByte::decode(buffer[0]);
         let data_size = first_mode_byte.decode_size();
 
-        // It is impossible for the mode decoded by the ModeByte to be invalid. That is why unreachable!() statements 
+        // It is impossible for the mode decoded by the ModeByte to be invalid. That is why unreachable!() statements
         // are okay here.
         let mode = match first_mode_byte.mode {
             Mode::REGISTER_MODE => Mode::Register { mode: RegisterMode::Register, register: first_mode_byte.register },
@@ -165,33 +158,33 @@ impl Operand {
             Mode::SECOND_MODE => {
                 input.read_exact(&mut buffer).map_err(|io| DecodeError::Io { io, decoder: DecodeIoError::SecondModeByte })?;
                 let second_mode_byte = ModeByte::decode(buffer[0]);
-                
+
                 let second_mode = match second_mode_byte.mode {
                     SecondMode::ARRAY_ADDRESSING_MODE => SecondMode::Array,
                     SecondMode::CONSTANT_MODE
-                    | SecondMode::RELATIVE_MODE 
+                    | SecondMode::RELATIVE_MODE
                     | SecondMode::ARRAY_IN_OBJECT_MODE => {
                         let constant_size = second_mode_byte.decode_size();
                         let constant = Self::decode_constant(input, constant_size)
                             .map_err(|io| DecodeError::Io { io, decoder: DecodeIoError::Constant { count: constant_size.size() }})?;
-                        
+
                         let mode = match second_mode_byte.mode {
                             SecondMode::CONSTANT_MODE => ConstantMode::Constant,
                             SecondMode::RELATIVE_MODE => ConstantMode::Relative,
                             SecondMode::ARRAY_ADDRESSING_MODE => ConstantMode::ArrayInObject,
                             _ => unreachable!()
                         };
-                        
+
                         SecondMode::ConstantBased { mode, constant }
                     },
                     _ => unreachable!()
                 };
-                
+
                 Mode::Second { mode: second_mode, base_register: first_mode_byte.register, index_register: second_mode_byte.register }
             },
             _ => unreachable!()
         };
-        
+
         Ok(Self { mode, data_size })
     }
 
