4909f5b0cacbefce5cdb5c6f73ede61685f68d5d
= <rayyankhan1428h@gmail.com>
2024-07-28 14:26:55 -0400

    feat(number) Added signed type.

--- src/math/dynamic_number.rs
+++ src/math/dynamic_number.rs
@@ -39,65 +39,117 @@ impl Size {
     }
 }
 
-impl From<DynamicNumber> for Size {
-    fn from(value: DynamicNumber) -> Self {
+impl From<Unsigned> for Size {
+    fn from(value: Unsigned) -> Self {
         match value {
-            DynamicNumber::U8(_) => Self::U8,
-            DynamicNumber::U16(_) => Self::U16,
-            DynamicNumber::U32(_) => Self::U32,
-            DynamicNumber::U64(_) => Self::U64
+            Unsigned::U8(_) => Self::U8,
+            Unsigned::U16(_) => Self::U16,
+            Unsigned::U32(_) => Self::U32,
+            Unsigned::U64(_) => Self::U64
         }
     }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-pub enum DynamicNumber {
+pub enum Unsigned {
     U8(u8),
     U16(u16),
     U32(u32),
     U64(u64)
 }
 
-impl From<DynamicNumber> for u8 {
-    fn from(value: DynamicNumber) -> Self {
+impl From<Unsigned> for u8 {
+    fn from(value: Unsigned) -> Self {
         match value {
-            DynamicNumber::U8(value) => value,
-            DynamicNumber::U16(value) => value as u8,
-            DynamicNumber::U32(value) => value as u8,
-            DynamicNumber::U64(value) => value as u8,
+            Unsigned::U8(value) => value,
+            Unsigned::U16(value) => value as u8,
+            Unsigned::U32(value) => value as u8,
+            Unsigned::U64(value) => value as u8,
         }
     }
 }
 
-impl From<DynamicNumber> for u16 {
-    fn from(value: DynamicNumber) -> Self {
+impl From<Unsigned> for u16 {
+    fn from(value: Unsigned) -> Self {
         match value {
-            DynamicNumber::U8(value) => value as u16,
-            DynamicNumber::U16(value) => value,
-            DynamicNumber::U32(value) => value as u16,
-            DynamicNumber::U64(value) => value as u16,
+            Unsigned::U8(value) => value as u16,
+            Unsigned::U16(value) => value,
+            Unsigned::U32(value) => value as u16,
+            Unsigned::U64(value) => value as u16,
         }
     }
 }
 
-impl From<DynamicNumber> for u32 {
-    fn from(value: DynamicNumber) -> Self {
+impl From<Unsigned> for u32 {
+    fn from(value: Unsigned) -> Self {
         match value {
-            DynamicNumber::U8(value) => value as u32,
-            DynamicNumber::U16(value) => value as u32,
-            DynamicNumber::U32(value) => value,
-            DynamicNumber::U64(value) => value as u32,
+            Unsigned::U8(value) => value as u32,
+            Unsigned::U16(value) => value as u32,
+            Unsigned::U32(value) => value,
+            Unsigned::U64(value) => value as u32,
         }
     }
 }
 
-impl From<DynamicNumber> for u64 {
-    fn from(value: DynamicNumber) -> Self {
+impl From<Unsigned> for u64 {
+    fn from(value: Unsigned) -> Self {
         match value {
-            DynamicNumber::U8(value) => value as u64,
-            DynamicNumber::U16(value) => value as u64,
-            DynamicNumber::U32(value) => value as u64,
-            DynamicNumber::U64(value) => value
+            Unsigned::U8(value) => value as u64,
+            Unsigned::U16(value) => value as u64,
+            Unsigned::U32(value) => value as u64,
+            Unsigned::U64(value) => value
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Signed {
+    I8(i8),
+    I16(i16),
+    I32(i32),
+    I64(i64)
+}
+
+impl From<Signed> for i8 {
+    fn from(value: Signed) -> Self {
+        match value {
+            Signed::I8(value) => value,
+            Signed::I16(value) => value as i8,
+            Signed::I32(value) => value as i8,
+            Signed::I64(value) => value as i8,
+        }
+    }
+}
+
+impl From<Signed> for i16 {
+    fn from(value: Signed) -> Self {
+        match value {
+            Signed::I8(value) => value as i16,
+            Signed::I16(value) => value,
+            Signed::I32(value) => value as i16,
+            Signed::I64(value) => value as i16,
+        }
+    }
+}
+
+impl From<Signed> for i32 {
+    fn from(value: Signed) -> Self {
+        match value {
+            Signed::I8(value) => value as i32,
+            Signed::I16(value) => value as i32,
+            Signed::I32(value) => value,
+            Signed::I64(value) => value as i32,
+        }
+    }
+}
+
+impl From<Signed> for i64 {
+    fn from(value: Signed) -> Self {
+        match value {
+            Signed::I8(value) => value as i64,
+            Signed::I16(value) => value as i64,
+            Signed::I32(value) => value as i64,
+            Signed::I64(value) => value
         }
     }
 }
