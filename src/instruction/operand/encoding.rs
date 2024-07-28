60c77023a4fed9505b22924794917d1370642192
= <rayyankhan1428h@gmail.com>
2024-07-27 20:55:32 -0400

    feat(operand) Support for decoding an operand from a binary stream.

--- src/instruction/operand/encoding.rs
+++ src/instruction/operand/encoding.rs
@@ -3,17 +3,18 @@ mod test;
 
 use std::io;
 use std::io::{Read, Write};
+use thiserror::Error;
 use crate::instruction::operand::{ConstantMode, EncodedModes, EncodedRegisters, InvalidModeError, Mode, Operand, RegisterMode, SecondMode};
 use crate::math::dynamic_number::{DynamicNumber, Size};
 
 impl SecondMode {
     pub fn encode_mode(self) -> u8 {
         match self {
-            Self::Array => Self::ARRAY_ADDRESSING_SECOND_MODE,
+            Self::Array => Self::ARRAY_ADDRESSING_MODE,
             Self::ConstantBased { mode, .. } => match mode {
-                ConstantMode::Constant => Self::CONSTANT_SECOND_MODE,
-                ConstantMode::Relative => Self::RELATIVE_SECOND_MODE,
-                ConstantMode::ArrayInObject => Self::ARRAY_IN_OBJECT_SECOND_MODE
+                ConstantMode::Constant => Self::CONSTANT_MODE,
+                ConstantMode::Relative => Self::RELATIVE_MODE,
+                ConstantMode::ArrayInObject => Self::ARRAY_IN_OBJECT_MODE
             }
         }
     }
@@ -21,7 +22,7 @@ impl SecondMode {
 
 impl Mode {
     /// Encode this operand based on its mode.
-    pub fn encode_mode(self) -> EncodedModes {
+    fn encode_mode(self) -> EncodedModes {
         match self {
             Mode::Register { mode, .. } => match mode {
                 RegisterMode::Register => EncodedModes(Self::REGISTER_MODE, None),
@@ -29,38 +30,18 @@ impl Mode {
             },
             Mode::Constant { .. } => EncodedModes(Self::CONSTANT_MODE, None),
             Mode::Second { mode, .. } => match mode {
-                SecondMode::Array => EncodedModes(Self::SECOND_MODE, Some(SecondMode::ARRAY_ADDRESSING_SECOND_MODE)),
+                SecondMode::Array => EncodedModes(Self::SECOND_MODE, Some(SecondMode::ARRAY_ADDRESSING_MODE)),
                 SecondMode::ConstantBased { mode, .. } => match mode {
-                    ConstantMode::Constant => EncodedModes(Self::SECOND_MODE, Some(SecondMode::CONSTANT_SECOND_MODE)),
-                    ConstantMode::Relative => EncodedModes(Self::SECOND_MODE, Some(SecondMode::RELATIVE_SECOND_MODE)),
-                    ConstantMode::ArrayInObject => EncodedModes(Self::SECOND_MODE, Some(SecondMode::ARRAY_IN_OBJECT_SECOND_MODE))
+                    ConstantMode::Constant => EncodedModes(Self::SECOND_MODE, Some(SecondMode::CONSTANT_MODE)),
+                    ConstantMode::Relative => EncodedModes(Self::SECOND_MODE, Some(SecondMode::RELATIVE_MODE)),
+                    ConstantMode::ArrayInObject => EncodedModes(Self::SECOND_MODE, Some(SecondMode::ARRAY_IN_OBJECT_MODE))
                 }
             }
         }
     }
-
-    pub fn decode_register_mode(first_mode: u8, register: u8) -> Result<Self, InvalidModeError> {
-        let mode_variant = match first_mode {
-            Self::REGISTER_MODE => RegisterMode::Register,
-            Self::DEREFERENCE_REGISTER_MODE => RegisterMode::Dereference,
-            _ => return Err(InvalidModeError)
-        };
-
-        Ok(Self::Register { mode: mode_variant, register })
-    }
-
-    pub fn decode_constant_mode(second_mode: u8, constant: DynamicNumber, base_register: u8, index_register: u8) -> Result<Self, InvalidModeError> {
-        let mode_variant = match second_mode {
-            SecondMode::CONSTANT_SECOND_MODE => ConstantMode::Constant,
-            SecondMode::RELATIVE_SECOND_MODE => ConstantMode::Relative,
-            SecondMode::ARRAY_IN_OBJECT_SECOND_MODE => ConstantMode::ArrayInObject,
-            _ => return Err(InvalidModeError)
-        };
-
-        Ok(Self::Second {
-            base_register, index_register,
-            mode: SecondMode::ConstantBased { mode: mode_variant, constant }
-        })
+    
+    fn is_second_mode(first_mode: u8) -> bool {
+        first_mode == Self::SECOND_MODE
     }
 }
 
@@ -100,6 +81,28 @@ impl ModeByte {
         let size = encoded & 0b000000_11;
         Self { register, mode, size }
     }
+    
+    fn decode_size(self) -> Size {
+        Size::from_power(self.size)
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum DecodeIoError {
+    #[error("Mandatory first mode byte was expected")]
+    FirstModeByte,
+    #[error("Second mode byte was expected")]
+    SecondModeByte,
+    #[error("{count} bytes expected for the constant")]
+    Constant { count: u8 }
+}
+
+#[derive(Debug, Error)]
+pub enum DecodeError {
+    #[error("Lock instruction used without memory operands")]
+    InvalidLock,
+    #[error("Could not decode due to IO Error")]
+    Io { #[source] io: io::Error, decoder: DecodeIoError },
 }
 
 impl Operand {
@@ -146,8 +149,78 @@ impl Operand {
         Ok(())
     }
     
-    pub fn decode(self, input: &mut impl Read) -> io::Result<()> {
+    pub fn decode(self, input: &mut impl Read) -> Result<Self, DecodeError> {
+        let mut buffer = [0u8; 1];
+        input.read_exact(&mut buffer).map_err(|io| DecodeError::Io { io, decoder: DecodeIoError::FirstModeByte })?;
         
-        Ok(())
+        let first_mode_byte = ModeByte::decode(buffer[0]);
+        let data_size = first_mode_byte.decode_size();
+
+        // It is impossible for the mode decoded by the ModeByte to be invalid. That is why unreachable!() statements 
+        // are okay here.
+        let mode = match first_mode_byte.mode {
+            Mode::REGISTER_MODE => Mode::Register { mode: RegisterMode::Register, register: first_mode_byte.register },
+            Mode::DEREFERENCE_REGISTER_MODE => Mode::Register { mode: RegisterMode::Dereference, register: first_mode_byte.register },
+            Mode::CONSTANT_MODE => {
+                let constant = Self::decode_constant(input, data_size)
+                    .map_err(|io| DecodeError::Io { io, decoder: DecodeIoError::Constant { count: data_size.size() }})?;
+                Mode::Constant { constant: u64::from(constant) }
+            },
+            Mode::SECOND_MODE => {
+                input.read_exact(&mut buffer).map_err(|io| DecodeError::Io { io, decoder: DecodeIoError::SecondModeByte })?;
+                let second_mode_byte = ModeByte::decode(buffer[0]);
+                
+                let second_mode = match second_mode_byte.mode {
+                    SecondMode::ARRAY_ADDRESSING_MODE => SecondMode::Array,
+                    SecondMode::CONSTANT_MODE
+                    | SecondMode::RELATIVE_MODE 
+                    | SecondMode::ARRAY_IN_OBJECT_MODE => {
+                        let constant_size = second_mode_byte.decode_size();
+                        let constant = Self::decode_constant(input, constant_size)
+                            .map_err(|io| DecodeError::Io { io, decoder: DecodeIoError::Constant { count: constant_size.size() }})?;
+                        
+                        let mode = match second_mode_byte.mode {
+                            SecondMode::CONSTANT_MODE => ConstantMode::Constant,
+                            SecondMode::RELATIVE_MODE => ConstantMode::Relative,
+                            SecondMode::ARRAY_ADDRESSING_MODE => ConstantMode::ArrayInObject,
+                            _ => unreachable!()
+                        };
+                        
+                        SecondMode::ConstantBased { mode, constant }
+                    },
+                    _ => unreachable!()
+                };
+                
+                Mode::Second { mode: second_mode, base_register: first_mode_byte.register, index_register: second_mode_byte.register }
+            },
+            _ => unreachable!()
+        };
+        
+        Ok(Self { mode, data_size })
+    }
+
+    fn decode_constant(input: &mut impl Read, size: Size) -> io::Result<DynamicNumber> {
+        Ok(match size {
+            Size::U8 => {
+                let mut buffer = [0u8; 1];
+                input.read_exact(&mut buffer)?;
+                DynamicNumber::U8(buffer[0])
+            },
+            Size::U16 => {
+                let mut buffer = [0u8; (u16::BITS / 8) as usize];
+                input.read_exact(&mut buffer)?;
+                DynamicNumber::U16(u16::from_le_bytes(buffer))
+            },
+            Size::U32 => {
+                let mut buffer = [0u8; (u32::BITS / 8) as usize];
+                input.read_exact(&mut buffer)?;
+                DynamicNumber::U32(u32::from_le_bytes(buffer))
+            },
+            Size::U64 => {
+                let mut buffer = [0u8; (u64::BITS / 8) as usize];
+                input.read_exact(&mut buffer)?;
+                DynamicNumber::U64(u64::from_le_bytes(buffer))
+            },
+        })
     }
 }
