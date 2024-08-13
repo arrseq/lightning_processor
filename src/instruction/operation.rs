a8547f35687ac777e8051d0ece81933f6b976a10
= <rayyankhan1428h@gmail.com>
2024-08-13 11:48:23 -0400

    feat(operation) Added LUT entries.

--- src/instruction/operation.rs
+++ src/instruction/operation.rs
@@ -1,18 +1,19 @@
-//! Enums of all operations for specific operand formats.
-
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub enum DualSource {
-    Compare
-}
-
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Destination {
     Unstack
 }
 
+impl Destination {
+    pub const MAPPINGS: [Destination; 1] = [Self::Unstack];
+}
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum DestinationSource {
     CopyRegisterToRegister
+}
+
+impl DestinationSource {
+    pub const MAPPINGS: [DestinationSource; 1] = [Self::CopyRegisterToRegister];
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -38,10 +39,25 @@ pub enum DestinationDualSource {
     DivideFloatVector,
 }
 
+impl DestinationDualSource {
+    pub const MAPPINGS: [DestinationDualSource; 16] = [
+        Self::Add, Self::Subtract, Self::Multiply, Self::Divide,
+        Self::AddFloat, Self::SubtractFloat, Self::MultiplyFloat, Self::DivideFloat,
+        Self::AddVector, Self::SubtractVector, Self::MultiplyVector, Self::DivideVector,
+        Self::AddFloatVector, Self::SubtractFloatVector, Self::MultiplyFloatVector, Self::DivideFloatVector,
+    ];
+}
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum DestinationTripleSource {
     MultiplyAndAdd,
     AddAndMultiply
+}
+
+impl DestinationTripleSource {
+    pub const MAPPINGS: [DestinationTripleSource; 2] = [
+        Self::MultiplyAndAdd, Self::AddAndMultiply,
+    ];
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -52,11 +68,24 @@ pub enum DualDestinationDualSource {
     DivideFloatVectorWithRemainder
 }
 
+impl DualDestinationDualSource {
+    pub const MAPPINGS: [DualDestinationDualSource; 4] = [
+        Self::DivideWithRemainder, Self::DivideFloatingWithRemainder,
+        Self::DivideVectorWithRemainder, Self::DivideFloatVectorWithRemainder,
+    ];
+}
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Memory {
     Call,
     ReleaseMemory,
     Branch
+}
+
+impl Memory {
+    pub const MAPPINGS: [Memory; 3] = [
+        Self::Call, Self::ReleaseMemory, Self::Branch,
+    ];
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -72,10 +101,26 @@ pub enum SourceMemory {
     AcquireMemoryQword
 }
 
+impl SourceMemory {
+    pub const MAPPINGS: [SourceMemory; 8] = [
+        Self::CopyMemoryByteToRegister, Self::CopyMemoryWordToRegister,
+        Self::CopyMemoryDwordToRegister, Self::CopyMemoryQwordToRegister,
+        Self::AcquireMemoryByte, Self::AcquireMemoryWord,
+        Self::AcquireMemoryDword, Self::AcquireMemoryQword,
+    ];
+}
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum DestinationMemory {
     CopyRegisterByteToMemory,
     CopyRegisterWordToMemory,
     CopyRegisterDwordToMemory,
     CopyRegisterQwordToMemory
+}
+
+impl DestinationMemory {
+    pub const MAPPINGS: [DestinationMemory; 4] = [
+        Self::CopyRegisterByteToMemory, Self::CopyRegisterWordToMemory,
+        Self::CopyRegisterDwordToMemory, Self::CopyRegisterQwordToMemory,
+    ];
 }
